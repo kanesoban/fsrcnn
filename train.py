ac50f1703cbfa0ed026859cbdf3174ac45284c6b
csaba.zsolnai <csaba.zsolnai@doclerholding.com>
2022-08-04 18:32:26 +0200

    add backpropagation, dataloaders

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
+import os
 from glob import glob
+import random
 
 import numpy as np
 from skimage.transform import resize
@@ -10,13 +12,37 @@ from torch.nn import MSELoss
 from fsrcnn.dataset import Dataset
 from fsrcnn.model import Model
 
-if __name__ == '__main__':
+
+upscaling_factor = 2
+mixed_precision_enabled = False
+
+
+def create_dataloaders():
     image_paths = glob('datasets/T91/*')
+    random.shuffle(image_paths)
 
-    upscaling_factor = 2
-    dataset = Dataset(image_paths, upscaling_factor=upscaling_factor)
+    train_set = int(0.8 * len(image_paths))
+    val_set = int(0.1 * len(image_paths))
+    train_paths = image_paths[:train_set]
+    val_paths = image_paths[train_set:train_set + val_set]
+    test_paths = image_paths[train_set + val_set:]
 
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    train_dataset = Dataset(train_paths, upscaling_factor=upscaling_factor)
+    train_dataloader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+
+    val_dataset = Dataset(val_paths, upscaling_factor=upscaling_factor)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+
+    test_dataset = Dataset(test_paths, upscaling_factor=upscaling_factor)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+    return train_dataloader, val_dataloader, test_dataloader
+
+
+if __name__ == '__main__':
+    train_dataloader, val_dataloader, test_dataloader = create_dataloaders()
+    torch.save(train_dataloader, 'train_dataloader.pt')
+    torch.save(val_dataloader, 'val_dataloader.pt')
+    torch.save(test_dataloader, 'test_dataloader.pt')
 
     # Detect if we have a GPU available
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -24,14 +50,41 @@ if __name__ == '__main__':
     optimizer = torch.optim.RMSprop(model.parameters())
     criterion = MSELoss()
 
-    running_loss = 0.0
+    epoch_train_loss = 0.0
+    epoch_val_loss = 0.0
+    best_val_loss = np.inf
 
-    for batch_idx, batch in tqdm(enumerate(dataloader)):
-        print('High resolution image shape: {}'.format(batch['high_res_image'].shape))
-        print('Low resolution image shape: {}'.format(batch['low_res_image'].shape))
+    # Train for 1 epoch
+    model.train()
+    with torch.set_grad_enabled(True):
+        for batch_idx, batch in tqdm(enumerate(train_dataloader)):
+            high_res_image = batch['high_res_image'].float().to(device)
+            low_res_image = batch['low_res_image'].float().to(device)
 
-        # track history if only in train
-        with torch.set_grad_enabled(True):
+            with torch.cuda.amp.autocast(enabled=False):
+                outputs = model(low_res_image.float())
+                # This resizing 'hack' is needed because the deconvolved output and the high resolution image are not exactly the same shape
+                high_res_image = np.expand_dims(resize(np.squeeze(high_res_image.cpu().numpy().transpose((0, 2, 3, 1))),
+                                                       outputs.shape[-2:]).transpose((2, 0, 1)), axis=0)
+                high_res_image = torch.from_numpy(high_res_image).float().to(device)
+                loss = criterion(outputs, high_res_image)
+
+            # Backpropagate losses
+            # TODO: handle scaler?
+            loss.backward()
+
+            # Apply updates
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # statistics
+            current_loss = loss.item() * low_res_image.size(0)
+            epoch_train_loss += current_loss
+
+    # Validate
+    model.eval()
+    with torch.set_grad_enabled(False):
+        for batch_idx, batch in tqdm(enumerate(val_dataloader)):
             high_res_image = batch['high_res_image'].float().to(device)
             low_res_image = batch['low_res_image'].float().to(device)
 
@@ -45,6 +98,11 @@ if __name__ == '__main__':
 
             # statistics
             current_loss = loss.item() * low_res_image.size(0)
-            running_loss += current_loss
+            epoch_val_loss += current_loss
+            if epoch_val_loss < best_val_loss:
+                best_val_loss = epoch_val_loss
+                torch.save(model.state_dict(), 'model.pt')
+                torch.save(optimizer, 'optimizer.pt')
 
-    print('Average loss: {}'.format(running_loss / len(dataloader.dataset)))
+        print('Average training loss for epoch: {}'.format(epoch_train_loss / len(train_dataloader.dataset)))
+        print('Average validation loss for epoch: {}'.format(epoch_val_loss / len(val_dataloader.dataset)))
